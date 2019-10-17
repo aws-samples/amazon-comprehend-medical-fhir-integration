abcc84a5c7b9dafafd25fd3ef4b974f974414f89
Mithun Mallick <mithumal@amazon.com>
2019-10-16 22:16:18 -0700

    updated path

--- resources/post-test-data.sh
+++ resources/post-test-data.sh
@@ -1,4 +1,4 @@
-for entry in "/home/ec2-user/environment/synthea/output/fhir_stu3"/*.json
+for entry in "/home/ec2-user/environment/amazon-comprehend-medical-fhir-integration/test-data/master-patient-data"/*.json
 do
   # include the cognito id token and make the endpoint as a parameter
   curl -H "Content-Type: application/fhir+json" -H "Authorization: <<IDToken>>" -i --data "@$entry" <<API_END_POINT>>Bundle
